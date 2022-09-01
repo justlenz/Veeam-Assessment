28d39f07c114fdec66209a316d69a826f402d452
justlenz <just.lenz@hotmail.de>
2022-09-01 16:49:32 +0200

    Console output

--- Synchronization-Script.ps1
+++ Synchronization-Script.ps1
@@ -1,4 +1,5 @@
-﻿$date1 = Get-Date -Format yyyy-MM-dd
+﻿#Dates
+$date1 = Get-Date -Format yyyy-MM-dd
 $date2 = (Get-Date).toString("yyyy-MM-dd HH:mm:ss")
 
 #Paths
@@ -19,16 +20,34 @@ $bTargetFiles = Get-ChildItem -Path $bTarget -Recurse -Name
 $diffs_left = Compare-Object @($aSourceFiles | Select-Object) @($bTargetFiles | Select-Object) -PassThru | Where-Object {$_.SideIndicator -eq "<="}
 $diffs_right = Compare-Object @($aSourceFiles | Select-Object) @($bTargetFiles | Select-Object) -PassThru | Where-Object {$_.SideIndicator -eq "=>"}
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+
 #Copy Files From Source
 foreach($file in $diffs_left){
     Copy-Item $aSource\$file -Destination $bTarget -Recurse -Force
-    $date2 + " COPY " + $aSource + "\" + $file + " TO " + $bTarget | Add-Content $logFile
+
+    #Output String
+    $outString = $date2 + " COPY " + $aSource + "\" + $file + " TO " + $bTarget
+
+    #Console Output
+    Write-Host $outString -ForegroundColor Yellow 
+
+    #Write To Log
+    $outString | Add-Content $logFile
     "-------------------" | Add-Content $logFile
 }
 
 #Delete Files From Target
 foreach($file in $diffs_right){
     Remove-Item $bTarget\$file -Recurse
-    $date2 + " DELETE " + $bTarget + "\" + $file | Add-Content $logFile
+
+    #Output String
+    $outString = $date2 + " DELETE " + $bTarget + "\" + $file
+
+    #Console Output
+    Write-Host $outString -ForegroundColor Red
+
+    #Write To Log
+    $outString | Add-Content $logFile
     "-------------------" | Add-Content $logFile
 }
