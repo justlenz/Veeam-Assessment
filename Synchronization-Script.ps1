2e1d2ee1f89e91a2161b85d558cae16340f431df
justlenz <47809339+justlenz@users.noreply.github.com>
2022-09-01 13:47:08 +0200

    Param & Copy

---
+++ Synchronization-Script.ps1
@@ -0,0 +1,19 @@
+﻿#Folder Paths
+$aSource = "C:\Users\Just\Documents\_Veeam_Assessment\aSource"
+$bTarget = "C:\Users\Just\Documents\_Veeam_Assessment\bTarget"
+
+#Logs Path
+$cLogs = "C:\Users\Just\Documents\_Veeam_Assessment\cLogs"
+
+#Current Date
+$dDate = Get-Date -Format yyyy-MM-dd
+
+#Check Target Folder
+if(Test-Path $bTarget\$dDate){}
+    else{New-Item -Path $bTarget\$dDate -ItemType directory}
+
+#Copy Items from source to target
+foreach($file in $aSource){
+    Copy-Item $file\* -Destination $bTarget\$dDate -Recurse -Force
+}
+
